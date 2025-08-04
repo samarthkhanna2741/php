f3a561f0ca73d8ed864eb5f9c4427c790f575192
Samarth Khanna <145190043+samarthkhanna2741@users.noreply.github.com>
2025-08-04 14:11:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Index</title>
  </head>
  <body>
- <?php echo '<p>Hello world from PHP! My name is Samarth</p'; ?> 
+ <?php echo '<p>Hello world from PHP! My name is Samarth Khanna 1234567890</p'; ?> 
  </body>
 </html>
